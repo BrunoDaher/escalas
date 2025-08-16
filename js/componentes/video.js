fff2c14e38789d6bef8453bec5548c8e083e2b73
brunus9 <brunus9@gmail.com>
2025-08-15 21:00:42 -0300

    videoMobileOn

--- js/componentes/video.js
+++ js/componentes/video.js
@@ -125,7 +125,7 @@ export default class VideoObj {
              
         return `
            <div id='videoControl' 
-                    class='${(aux.infoNavegador.mobile && !aux.infoNavegador.tablet) ? 'off':'on'} 
+                    class='${(!aux.infoNavegador.tablet) ? 'off':'on'} 
                     gap2 p-2 flexCenter abs' >
                     ${this.botoesControle().map(btn => `
                             <span id='${btn.id}' 
