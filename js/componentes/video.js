ee9ba281bfd7e6e1064332d66f790868f58cbf8c
brunus9 <brunus9@gmail.com>
2026-04-22 01:14:09 -0300

    ajustes8urls

--- js/componentes/video.js
+++ js/componentes/video.js
@@ -196,7 +196,7 @@ export default class VideoObj {
                             preload="metadata"
                             autoplay
                             >
-                        <source src="data/abertura.mp4" type="video/mp4">
+                        <source src="./data/abertura.mp4" type="video/mp4">
                         Seu navegador não suporta a tag de vídeo.
                     </video>
                 
@@ -231,7 +231,7 @@ export default class VideoObj {
           this.currentVideo.src = urlDados;
         }
         else{
-            this.currentVideo.src = `data/pratica.mp4`;
+            this.currentVideo.src = `./data/pratica.mp4`;
         }
     } 
 
