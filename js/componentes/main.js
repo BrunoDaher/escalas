cc0d6129fc9c3899e118e13237e8b0d0686cbf3d
brunus9 <brunus9@gmail.com>
2026-04-26 23:34:07 -0300

    domingao3

--- js/componentes/main.js
+++ js/componentes/main.js
@@ -75,7 +75,6 @@ export class Main extends Aux{
         let buttons = this.sections.map(btn => `
             <span id=${btn.name}  
                 class="navBtn w-100 grid colorB f2vh" 
-               
                 data-panel="${btn.id}">
                 <i class="${btn.icon}"></i>
                 <a style='text-transform:capitalize'>${btn.name}</a>
