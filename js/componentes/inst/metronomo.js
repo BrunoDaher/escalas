bace4d3ee81d8c2865e9bd308b21a3d2c148877a
brunus9 <brunus9@gmail.com>
2026-06-17 15:42:55 -0300

    update17-06

--- js/componentes/inst/metronomo.js
+++ js/componentes/inst/metronomo.js
@@ -98,9 +98,8 @@ export class Metronomo{
                             <legend class='colorD f3vh flexCenter justCenter p-1' id='lbpm' for='bpm'>Pulsos</legend>
                             <div class="comp justContBetween w-full px-2 flexrow gap2 ">
                                 ${[1, 2, 3, 4].map(i => `
-                                        <button id="pls${i}" count="${i}" class="compCount flexCenter f1rem  p-1 ${i === 4 ? 'active' : ''}">
-                                            <i class='ind bi-hand-index-thumb f3vh'></i>
-                                            <a class='f2vh colorB'>${i}</a>
+                                        <button  id="pls${i}" count="${i}" class="compCount flexColBetween f1rem  p-1 ${i === 4 ? 'active' : ''}">
+                                            ${i}
                                         </button>
                                 `).join('')}
                             </div>
